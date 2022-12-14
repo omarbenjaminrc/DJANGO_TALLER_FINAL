0d72bed4f0c96052b7549dcf70181f3d3de0b443
Omar Benjamin Ramirez Castillo <omarbenjaminrc@gmail.com>
2022-12-14 20:00:26 -0300

    borrar el estado

--- django_seminario_app/models.py
+++ django_seminario_app/models.py
@@ -9,7 +9,6 @@ estado = [
     ('anulada','ANULADA'),
     ('no asisten','NO ASISTEN')
 ]
-personas = [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11),(12,12),(13,13),(14,14),(15,15)]
 
 class Inscripcion(models.Model):
     rut                 = models.CharField(max_length=12)  
